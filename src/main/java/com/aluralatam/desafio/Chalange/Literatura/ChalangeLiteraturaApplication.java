5789a28ea018a4ca6343c479c417ed81101a4004
kevin <kevinbrrs96@gmail.com>
2024-12-27 10:33:42 -0300

    Inicializando proyecto

---
+++ src/main/java/com/aluralatam/desafio/Chalange/Literatura/ChalangeLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.aluralatam.desafio.Chalange.Literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalangeLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalangeLiteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluralatam/desafio/Chalange/Literatura/ChalangeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluralatam.desafio.Chalange.Literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalangeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
